1f82fbb8075169a00771a25e0d365d20f726c63f
Sbahle03 <mzobesbahle03@gmail.com>
2024-11-11 11:48:26 +0200

    added new js

---
+++ practice.js
@@ -0,0 +1,28 @@
+//  JavaScripts is about how yor web page behave.
+// The  best practice is to put your js script at the bottom of the of the body section ! -->
+ // The below is a statement, as statement is piece of code that expresse  a action , in this case we want the cosole to disply hello world!.hello world is a string , because it is between quotes
+console.log("Hello, World");
+
+let x ="Cat";
+console.log(x)
+// note the vriable name , shulod be meaning ful , and donot use js keyword
+// the variable cnnot start with a number
+// cannot start with a space or hypen
+// variable name are case sensitive.
+// if declrig multiple variables  , declare in a new line.
+
+
+// Constant ! if your donot need to reassign a variable.
+const interestRate = 0.3;
+interestRate =1;
+console.log(interestRate)
+
+// Data types , the are reference type and primitive or values types
+let name = "Sbahle"; // this is a string literal
+let age = 21 ; //  Number  Literal
+let isApproved = true; // Boolean literal use case if i want to some logic
+let y; // Undefiend 
+let r = null // we use null when we want to explicity clear the value to a variable
+
+// Dynamic typing - JS is a dynamic languange emaning the type of  data a variable  is can change 
+typeof name
---
+++ variables.js
@@ -0,0 +1,90 @@
+// kebabcse -. my-array (only use in vue
+// Variables on Js
+
+// let  name = "Sbahle" 
+
+
+
+// const name ="Sbahle"
+// console.log(name);
+
+
+// var name = "Sbahle"
+// console.log(name);
+
+// Strings
+
+// let name
+// console.log(name);
+
+// Number
+// let number = 10.5
+// console.log(number);
+// Boolean Value
+
+// let value =true //false
+// console.log(name);
+// let value = null
+// console.log( `My name is ${name}`);
+
+// Arrays are = to lsit
+
+// // To access the value in the Array , we must use the indexiting
+// let my_Array =["anything" , " 2"  , "true" ,"string" ]   
+// console.log(myArray[0]);
+
+// camelcase -> myArray(always used in JS)
+// Pascalcase  -> myArray
+// kebabcse -. my-array (only use in vue
+
+
+
+// object literal -declare (make) an object
+// let table ={
+//     material:"wood",
+//     no_of_legs:4,
+//     occupied: true  
+// }
+
+// // bracket notation
+// console.log( table["material"]);
+
+// // dot notation
+// console.log(table.material);
+// destructuring
+// let {material}= table //wood      // reduntant 
+// let {occupied}= table //wood
+// console.log(material);
+
+// functions
+// () -. parentheses
+// [] --. brackets
+// -->braces
+// function greeting(a , b ){
+    // a&b are paramenters
+    // console.log("This is a greeting  function" + ( a + b))
+    
+// }
+// 3 and 4 are arguements
+// greeting( 3 , 4)
+
+// ++num1 pre increment
+// num++ post increment
+// --num1 pre decrement
+// num1-- post increment
+
+
+
+
+let myString = "Something"
+console.log(myString.length);  // used to check if a user the length of the user input or string.
+console.log(myString.toLowerCase); // used to covert from uppercase to lowercase
+console.log(myString.toUpperCase); // used to covert string to uppercase
+let split = myString.toLowerCase().split('e');
+console.log(split ); // it convet a string in a array!
+
+
+
+
+
+
